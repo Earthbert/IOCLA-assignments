fcca298d39a08d24b2ef6a18f3beea9f863c3138
Earthbert <daraban_timotei@tutanota.com>
2022-04-02 17:01:00 +0300

    doesn't really work

--- main.c
+++ main.c
@@ -9,9 +9,9 @@
 int add_last(void **arr, int *len, data_structure *data)
 {
 	char *last_p = *arr;  // will point to the end of the arr
-	for (int i = 0; i < len; i++) {
-		data_structure *curr = (data_structure *)last_p;
-		last_p = last_p + sizeof(head) + curr->header->len;
+	for (int i = 0; i < *len; i++) {
+		head *curr = (head *)last_p;
+		last_p = last_p + sizeof(head) + curr->len;
 	}
 
 	u_int64_t arr_size = (u_int64_t)last_p - (u_int64_t)*arr;
@@ -25,7 +25,7 @@ int add_last(void **arr, int *len, data_structure *data)
 	memcpy(tmp + arr_size + sizeof(head), data->data, data->header->len);
 
 	*arr = (void *)tmp;
-	*len++;
+	*len = *len + 1;
 	return 1;
 }
 
@@ -33,11 +33,11 @@ int add_at(void **arr, int *len, data_structure *data, int index)
 {
 	char *last_p = *arr;  // will point to the end of the arr
 	char *add_p;  // will point to the data from index
-	for (int i = 0; i < len; i++) {
+	for (int i = 0; i < *len; i++) {
 		if (i == index)
 			add_p = last_p;
-		data_structure *curr = (data_structure *)last_p;
-		last_p = last_p + sizeof(head) + curr->header->len;
+		head *curr = (head *)last_p;
+		last_p = last_p + sizeof(head) + curr->len;
 	}
 
 	// total nr of bytes of arr
@@ -52,7 +52,7 @@ int add_at(void **arr, int *len, data_structure *data, int index)
 	memcpy(tmp + (arr_size - add_size), add_p, add_size);
 
 	*arr = (void *)tmp;
-	*len++;
+	*len = *len + 1;
 	return 1;
 }
 
@@ -62,15 +62,15 @@ void print_data(void *data, u_char type) {
 		char *name1 = data;
 		int8_t *bac1 = data + strlen(name1);
 		int8_t *bac2 = bac1 + sizeof(int8_t);
-		char *name2 = bac2 + sizeof(int8_t);
+		char *name2 = (char *)bac2 + sizeof(int8_t);
 		printf("%s pentru %s\n", name1, name2);
 		printf("%d\n%d\n", *bac1, *bac2);
 	}
 	if (type == 2) {
 		char *name1 = data;
 		int16_t *bac1 = data + strlen(name1);
-		int32_t *bac2 = bac1 + sizeof(int16_t);
-		char *name2 = bac2 + sizeof(int32_t);
+		int32_t *bac2 = (int32_t *)bac1 + sizeof(int16_t);
+		char *name2 = (char *)bac2 + sizeof(int32_t);
 		printf("%s pentru %s\n", name1, name2);
 		printf("%d\n%d\n", *bac1, *bac2);
 	}
@@ -78,7 +78,7 @@ void print_data(void *data, u_char type) {
 		char *name1 = data;
 		int32_t *bac1 = data + strlen(name1);
 		int32_t *bac2 = bac1 + sizeof(int32_t);
-		char *name2 = bac2 + sizeof(int32_t);
+		char *name2 = (char *)bac2 + sizeof(int32_t);
 		printf("%s pentru %s\n", name1, name2);
 		printf("%d\n%d\n", *bac1, *bac2);
 	}
@@ -92,34 +92,34 @@ void find(void *data_block, int len, int index)
 		index = len - 1;
 
 	char *print_p = data_block;
-	for (int i = 0; i < len; i++) {
-		data_structure *curr = (data_structure *)print_p;
-		print_p = print_p + sizeof(head) + curr->header->len;
+	for (int i = 0; i < index; i++) {
+		head *curr = (head *)print_p;
+		print_p = print_p + sizeof(head) + curr->len;
 	}
 
-	data_structure *data_s = (data_structure *)print_p;
-	print_data(print_p + sizeof(head), data_s->header->type);
+	head *data_s = (head *)print_p;
+	print_data(print_p + sizeof(head), data_s->type);
 }
 
 int delete_at(void **arr, int *len, int index)
 {
 	if (index < 0)
-		return;
-	if (index >= len)
-		index = len - 1;
+		return 0;
+	if (index >= *len)
+		index = *len - 1;
 
 	char *last_p;
 	char *del_p;
-	for (int i = 0; i < len; i++) {
+	for (int i = 0; i < *len; i++) {
 		if (i == index)
 			del_p = last_p;
-		data_structure *curr = (data_structure *)last_p;
-		last_p = last_p + sizeof(head) + curr->header->len;
+		head *curr = (head *)last_p;
+		last_p = last_p + sizeof(head) + curr->len;
 	}
 
 	u_int64_t arr_size = (uint64_t)last_p - (uint64_t)*arr;
-	data_structure *del_s = (data_structure *)del_p;
-	u_int64_t del_size = sizeof(head) + del_s->header->len;
+	head *del_s = (head *)del_p;
+	u_int64_t del_size = sizeof(head) + del_s->len;
 	u_int64_t remain_size = arr_size - ((uint64_t)del_p -(uint64_t)*arr);
 
 	char *buffer = calloc(1, remain_size);
@@ -137,13 +137,13 @@ int delete_at(void **arr, int *len, int index)
 	memcpy(buffer, tmp + (last_p - del_p), remain_size);
 
 	*arr = tmp;
-	*len--;
+	*len = *len - 1;
 	return 1;
 }
 
 int pack_data(data_structure *data_s) {
 	u_char type;
-	scanf("%d", &type);
+	scanf("%hhd", &type);
 	data_s->header->type = type;
 	char *name1 = calloc(MAX_STRING_SIZE, sizeof(char));
 	if (!name1)
@@ -153,10 +153,8 @@ int pack_data(data_structure *data_s) {
 		return 0;
 	if (type == 1) {
 		int8_t bac1, bac2;
-		scanf("%s %d %d %s", name1, &bac1, &bac2, name2);
+		scanf("%s %hhd %hhd %s", name1, &bac1, &bac2, name2);
 		char *data = data_s->data;
-		if (data)
-			free(data);
 		data_s->header->len = strlen(name1) + strlen(name2) + sizeof(bac1) + sizeof(bac2);
 		data = calloc(1, data_s->header->len);
 		memcpy(data, name1, strlen(name1));
@@ -168,10 +166,8 @@ int pack_data(data_structure *data_s) {
 	if (type == 2) {
 		int16_t bac1;
 		int32_t bac2;
-		scanf("%s %d %d %s", name1, &bac1, &bac2, name2);
+		scanf("%s %hd %d %s", name1, &bac1, &bac2, name2);
 		char *data = data_s->data;
-		if (data)
-			free(data);
 		data_s->header->len = strlen(name1) + strlen(name2) + sizeof(bac1) + sizeof(bac2);
 		data = calloc(1, data_s->header->len);
 		memcpy(data, name1, strlen(name1));
@@ -184,8 +180,6 @@ int pack_data(data_structure *data_s) {
 		int32_t bac1, bac2;
 		scanf("%s %d %d %s", name1, &bac1, &bac2, name2);
 		char *data = data_s->data;
-		if (data)
-			free(data);
 		data_s->header->len = strlen(name1) + strlen(name2) + sizeof(bac1) + sizeof(bac2);
 		data = calloc(1, data_s->header->len);
 		memcpy(data, name1, strlen(name1));
@@ -196,6 +190,7 @@ int pack_data(data_structure *data_s) {
 	}
 	free(name1);
 	free(name2);
+	return 1;
 }
 
 int main() {
@@ -210,16 +205,19 @@ int main() {
 	data_s->header = calloc(1, sizeof(head));
 
 	while (1) {
+		printf("%d\n", len);
 		scanf("%s", command);
 		if (!strcmp(command, "insert")) {
 			pack_data(data_s);
 			add_last(&arr, &len, data_s);
+			free(data_s->data);
 		}
 		if (!strcmp(command, "insert_at")) {
 			int index;
 			scanf("%d", &index);
 			pack_data(data_s);
 			add_at(&arr, &len, data_s, index);
+			free(data_s->data);
 		}
 		if (!strcmp(command, "delete_at")) {
 			int index;
