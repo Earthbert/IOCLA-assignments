646c1b2bdbcc792262f8b2deaab574fe24f91185
Earthbert <daraban_timotei@tutanota.com>
2022-04-24 12:05:27 +0300

    Add solution for task 3, homework 2

--- Tema2/task-3/beaufort.asm
+++ Tema2/task-3/beaufort.asm
@@ -20,7 +20,43 @@ beaufort:
     ;; DO NOT MODIFY
     ;; TODO: Implement spiral encryption
     ;; FREESTYLE STARTS HERE
+    mov ecx, 0
+    mov esi, [ebp + 12]
+    mov edi, [ebp + 20]
+    mov edx, [ebp + 28]
+    xor eax, eax
 
+    ;; We move in the plain string
+mov_in_text:
+    ;; We put the char from the plain in bl
+    mov bl, byte [esi + ecx]
+
+    ;; We put the coresponding char from the key in bh
+    mov ax, cx
+    ;; To not go over the key lenght we do modulo len_key
+    div byte [ebp + 16]
+    mov al, ah
+    xor ah, ah
+    mov bh, byte [edi + eax]
+
+    ;; The difference between key char and plain char is the position of the 
+    ;; encypted char in the alphabet
+    sub bh, bl
+    
+    ;; Check if diff is below 0 and add 26 if it is
+    cmp bh, 0
+    jge bh_is_good
+    add bh, 26
+bh_is_good:
+
+    ;; We give the char the right ascii code
+    add bh, 65
+    ;; We put the char in the encypted string
+    mov byte [edx + ecx], bh
+
+    inc ecx
+    cmp [ebp + 8], ecx
+    jg mov_in_text
 
     ;; FREESTYLE ENDS HERE
     ;; DO NOT MODIFY
